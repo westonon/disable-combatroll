cd00e29c75390e39e6c91065b71385ceaba73c99
weston <117936288+westonon@users.noreply.github.com>
2023-04-09 17:58:00 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,12 @@
 fx_version 'cerulean'
 
+author 'weston#0001'
+description 'https://github.com/westonon/disable-combatroll/'
+
+
 game 'gta5'
 author 'Decept'
-description 'https://github.com/DeceptDevv/DeceptDisableCombatRoll'
+description 'https://github.com/westonon/disable-combatroll/'
 version '1.0.1'
 
 client_script 'main.lua'
